599921889f43970eb91012725ea0dec9c20f53e1
Archit Gupta <rarchitgupta@gmail.com>
2022-01-09 12:04:00 +0530

    Book 2 API complete

--- books2.py
+++ books2.py
@@ -1,7 +1,15 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
 from uuid import UUID
+from starlette.responses import JSONResponse
+
+from starlette.responses import JSONResponse
+
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
 
 
 class Book(BaseModel):
@@ -25,13 +33,32 @@ class Book(BaseModel):
         }
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1)
+    description: Optional[str] = Field(
+        None, title="Description of book", min_length=1, max_length=100
+    )
+
+
 app = FastAPI()
 
 books = []
 
 
+@app.exception_handler(NegativeNumberException)
+async def negative_exception_handler(request: Request, exception: Exception):
+    return JSONResponse(
+        status_code=418, content={"message": "Negative Number of Books entered"}
+    )
+
+
 @app.get("/")
 async def read_all_books(books_to_return: Optional[int] = None):
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if books_to_return and len(books) >= books_to_return > 0:
         i = 1
         new_books = []
@@ -44,6 +71,14 @@ async def read_all_books(books_to_return: Optional[int] = None):
 
 @app.get("/book/{book_id}")
 async def read_book(book_id: UUID):
+    for book in books:
+        if book.id == book_id:
+            return book
+    raise item_cannot_be_found_exception()
+
+
+@app.get("/book/rating/{book_id}", response_model=BookNoRating)
+async def read_book_no_rating(book_id: UUID):
     for book in books:
         if book.id == book_id:
             return book
@@ -72,10 +107,20 @@ async def delete_book(book_id: UUID):
     raise item_cannot_be_found_exception()
 
 
-@app.post("/")
+@app.get("/header")
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {"Random-header": random_header}
+
+
+@app.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     books.append(book)
     return book
+
+
+@app.post("/books/login")
+async def book_login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username, "password": password}
 
 
 def item_cannot_be_found_exception():
